e3dc963d03be03c8134f1fea9a83216497b95843
Uttam Nagvadiya <uttam.git835@gmail.com>
2023-07-10 22:33:09 +0530

    .

---
+++ ASP .NET/C - Sharp/001/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using _001;
+
+Console.WriteLine("Hello, World!\n");
+
+Fundamentals f =  new Fundamentals();
+
+//  print own details
+    f.displayDetails();
+
+//  get input from user
+//  f.getDetailsFromUser();
+
+//  calculate area of rectangle & circle.
+//  f.calculateArea();
+
+//  calculate temperature
+//  f.calculateTemperature();
+
+//  calculate simple intrest
+//  f.simpleIntrest();
+
+//  simple calculator
+//  f.simpleCalculator();
+
+//  maximum number of 3
+//  f.maximumNumber();
+
+//  swap without third variable
+//  f.swapWithoutThirdVariable();

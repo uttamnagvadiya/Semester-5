5c139d6aa1302c568659764cf91fed7de817ef64
Uttam Nagvadiya <uttam.git835@gmail.com>
2023-07-31 21:29:27 +0530

    .

--- ASP .NET/Demo_Project/Database/SelectProcedures.sql
+++ ASP .NET/Demo_Project/Database/SelectProcedures.sql
@@ -1,5 +1,5 @@
 
--- Create Procedure for Select all countries
+-- 1. Create Procedure for Select all countries
 CREATE PROCEDURE [dbo].[PR_Country_SelectAll]
 AS
 
@@ -13,7 +13,7 @@ FROM [dbo].[LOC_Country]
 ORDER BY [dbo].[LOC_Country].[CountryName]
 
 
--- Create Procedure for Select all states
+-- 2. Create Procedure for Select all states
 CREATE PROCEDURE [dbo].[PR_State_SelectAll]
 AS
 
@@ -31,7 +31,7 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
         ,[dbo].[LOC_State].[StateName]
 
 
--- Create Procedure for Select all cities
+-- 3. Create Procedure for Select all cities
 CREATE PROCEDURE [dbo].[PR_City_SelectAll]
 AS
 
@@ -53,10 +53,12 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 		,[dbo].[LOC_City].[CityName]
 
 
+--===========================================================================================================
 
--- Create Procedure for Select Country by PK
+
+-- 4. Create Procedure for Select Country by PK
 CREATE PROCEDURE [dbo].[PR_Country_SelectByPK]
-@CountryID int
+	@CountryID int
 AS
 
 SELECT [dbo].[LOC_Country].[CountryName]
@@ -70,9 +72,9 @@ WHERE [dbo].[LOC_Country].[CountryID] = @CountryID
 ORDER BY [dbo].[LOC_Country].[CountryName]
 
 
--- Create Procedure for Select State by PK
+-- 5. Create Procedure for Select State by PK
 CREATE PROCEDURE [dbo].[PR_State_SelectByPK]
-@StateID int
+	@StateID int
 AS
 
 SELECT [dbo].[LOC_State].[StateID]
@@ -90,9 +92,9 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
         ,[dbo].[LOC_State].[StateName]
 
 
--- Create Procedure for Select City by PK
+-- 6. Create Procedure for Select City by PK
 CREATE PROCEDURE [dbo].[PR_City_SelectByPK]
-@CityID int
+	@CityID int
 AS
 
 SELECT [dbo].[LOC_City].[CityID]
@@ -112,3 +114,160 @@ WHERE [dbo].[LOC_City].[CityID] = @CityID
 ORDER BY [dbo].[LOC_Country].[CountryName]
         ,[dbo].[LOC_State].[StateName]
 		,[dbo].[LOC_City].[CityName]
+
+
+
+--===========================================================================================================
+
+
+-- 7. Create Insert Procedure to add any new record for Country
+CREATE PROCEDURE [dbo].[PR_Country_Insert_Record]
+	@CountryName	varchar(100),
+	@CountryCode	varchar(50)
+
+AS
+INSERT INTO [dbo].[LOC_Country]
+(
+	[CountryName]
+   ,[CountryCode]
+)
+VALUES
+(
+	@CountryName,
+	@CountryCode
+)
+
+
+-- 8. Create Insert Procedure to add new record for State.
+CREATE PROCEDURE [dbo].[PR_State_Insert_Record]
+	@StateName	varchar(100),
+	@CountryID	int,
+	@StateCode	varchar(50)
+
+AS
+INSERT INTO [dbo].[LOC_State]
+(
+	[StateName]
+   ,[CountryID]
+   ,[StateCode]
+   
+)
+VALUES
+(
+	@StateName,
+	@CountryID,
+	@StateCode
+	
+)
+
+
+-- 9. Create Insert Procedure to add new record for City.
+CREATE PROCEDURE [dbo].[PR_City_Insert_Record]
+	@CityName	varchar(100),
+	@StateID	int,
+	@CountryID	int,
+	@CityCode	varchar(50)
+
+AS
+INSERT INTO [dbo].[LOC_City]
+(
+	[CityName]
+   ,[StateID]
+   ,[CountryID]
+   ,[Citycode]
+   
+)
+VALUES
+(
+	@CityName,
+	@StateID,
+	@CountryID,
+	@CityCode
+)
+
+
+--===========================================================================================================
+
+
+-- 10. Create Update Procedure to edit/modeify existing record for Country.
+CREATE PROCEDURE [dbo].[PR_Country_UpdateByPK]
+	@CountryID		int,
+	@CountryName	varchar(100),
+	@CountryCode	varchar(50)
+
+AS
+UPDATE [dbo].[LOC_Country]
+	
+	SET [CountryName] = @CountryName,
+		[CountryCode] = @CountryCode
+
+	WHERE [dbo].[LOC_Country].[CountryID] = @CountryID
+
+
+-- 11. Create Update Procedure to edit/modify existing record for State.
+CREATE PROCEDURE [dbo].[PR_State_UpdateByPK]
+	@StateID	int,
+	@StateName	varchar(100),
+	@CountryID	int,
+	@StateCode	varchar(50)
+
+AS
+UPDATE [dbo].[LOC_State]
+
+	SET [StateName] = @StateName,
+		[CountryID] = @CountryID,
+		[StateCode] = @StateCode
+
+	WHERE [dbo].[LOC_State].[StateID] = @StateID
+
+
+-- 12. Create Update Procedure to edit/modify existing record for City.
+CREATE PROCEDURE [dbo].[PR_City_UpdateByPK]
+	@CityID		int,
+	@CityName	varchar(100),
+	@StateID	int,
+	@CountryID	int,
+	@CityCode	varchar(50)
+
+AS
+UPDATE [dbo].[LOC_City]
+
+	SET [CityName] = @CityName,
+		[StateID] = @StateID,
+		[CountryID] = @CountryID,
+		[Citycode] = @CityCode
+
+	WHERE [dbo].[LOC_City].[CityID] = @CityID
+	
+
+--===========================================================================================================
+
+
+-- 13. Create Delete Procedure to delete record for Country.
+CREATE PROCEDURE [dbo].[PR_Country_DeleteByPK]
+	@CountryID	int
+
+AS
+
+DELETE FROM [dbo].[LOC_Country]
+WHERE [dbo].[LOC_Country].[CountryID] = @CountryID
+
+
+-- 14. Create Delete Procedure to delete record for State.
+CREATE PROCEDURE [dbo].[PR_State_DeleteByPK]
+	@StateID	int
+
+AS
+
+DELETE FROM [dbo].[LOC_State]
+WHERE [dbo].[LOC_State].[StateID] = @StateID
+
+
+-- 13. Create Delete Procedure to delete record for City.
+CREATE PROCEDURE [dbo].[PR_City_DeleteByPK]
+	@CityID	int
+
+AS
+
+DELETE FROM [dbo].[LOC_City]
+WHERE [dbo].[LOC_City].[CityID] = @CityID

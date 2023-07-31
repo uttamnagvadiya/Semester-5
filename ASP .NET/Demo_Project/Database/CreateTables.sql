e15300cb6b53ac361ce2bc993b9ef8c8343d4bf0
Uttam Nagvadiya <uttam.git835@gmail.com>
2023-07-31 19:11:19 +0530

    .

---
+++ ASP .NET/Demo_Project/Database/CreateTables.sql
@@ -0,0 +1,161 @@
+CREATE TABLE [dbo].[LOC_City](
+	[CityID] [int] IDENTITY(1,1) NOT NULL,
+	[CityName] [varchar](100) NOT NULL,
+	[StateID] [int] NOT NULL,
+	[CountryID] [int] NOT NULL,
+	[Citycode] [varchar](50) NOT NULL,
+	[CreationDate] [datetime] NOT NULL,
+	[Modified] [datetime] NOT NULL,
+ CONSTRAINT [PK_LOC_City] PRIMARY KEY CLUSTERED 
+(
+	[CityID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[LOC_Country]    Script Date: 24-Jul-23 9:49:15 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[LOC_Country](
+	[CountryID] [int] IDENTITY(1,1) NOT NULL,
+	[CountryName] [varchar](100) NOT NULL,
+	[CountryCode] [varchar](50) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Modified] [datetime] NOT NULL,
+ CONSTRAINT [PK_LOC_Country] PRIMARY KEY CLUSTERED 
+(
+	[CountryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[LOC_State]    Script Date: 24-Jul-23 9:49:15 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[LOC_State](
+	[StateID] [int] IDENTITY(1,1) NOT NULL,
+	[StateName] [varchar](100) NOT NULL,
+	[CountryID] [int] NOT NULL,
+	[StateCode] [varchar](50) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Modified] [datetime] NOT NULL,
+ CONSTRAINT [PK_LOC_State] PRIMARY KEY CLUSTERED 
+(
+	[StateID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[MST_Branch]    Script Date: 24-Jul-23 9:49:15 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[MST_Branch](
+	[BranchID] [int] IDENTITY(1,1) NOT NULL,
+	[BranchName] [varchar](100) NOT NULL,
+	[BranchCode] [varchar](100) NOT NULL,
+	[Created] [datetime] NOT NULL,
+	[Modified] [datetime] NOT NULL,
+ CONSTRAINT [PK_MST_Branch] PRIMARY KEY CLUSTERED 
+(
+	[BranchID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[MST_Student]    Script Date: 24-Jul-23 9:49:15 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[MST_Student](
+	[StudentID] [int] NOT NULL,
+	[BranchID] [int] NOT NULL,
+	[CityID] [int] NOT NULL,
+	[StudentName] [varchar](100) NOT NULL,
+	[MobileNoStudent] [varchar](100) NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[MobileNoFather] [varchar](100) NULL,
+	[Address] [varchar](500) NULL,
+	[BirthDate] [datetime] NULL,
+	[Age] [int] NULL,
+	[IsActive] [bit] NULL,
+	[Gender] [varchar](50) NULL,
+	[Password] [varchar](100) NULL,
+	[Created] [datetime] NOT NULL,
+	[Modified] [datetime] NOT NULL
+	
+ CONSTRAINT [PK_MST_Student] PRIMARY KEY CLUSTERED 
+(
+	[StudentID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
+GO
+ALTER TABLE [dbo].[LOC_City] ADD  CONSTRAINT [DF_LOC_City_CreationDate]  DEFAULT (getdate()) FOR [CreationDate]
+GO
+ALTER TABLE [dbo].[LOC_City] ADD  CONSTRAINT [DF_LOC_City_Modified]  DEFAULT (getdate()) FOR [Modified]
+GO
+ALTER TABLE [dbo].[LOC_Country] ADD  CONSTRAINT [DF_LOC_Country_Created]  DEFAULT (getdate()) FOR [Created]
+GO
+ALTER TABLE [dbo].[LOC_Country] ADD  CONSTRAINT [DF_LOC_Country_Modified]  DEFAULT (getdate()) FOR [Modified]
+GO
+ALTER TABLE [dbo].[LOC_State] ADD  CONSTRAINT [DF_LOC_State_Created]  DEFAULT (getdate()) FOR [Created]
+GO
+ALTER TABLE [dbo].[LOC_State] ADD  CONSTRAINT [DF_LOC_State_Modified]  DEFAULT (getdate()) FOR [Modified]
+GO
+ALTER TABLE [dbo].[MST_Branch] ADD  CONSTRAINT [DF_MST_Branch_Created]  DEFAULT (getdate()) FOR [Created]
+GO
+ALTER TABLE [dbo].[MST_Branch] ADD  CONSTRAINT [DF_MST_Branch_Modified]  DEFAULT (getdate()) FOR [Modified]
+GO
+ALTER TABLE [dbo].[MST_Student] ADD  CONSTRAINT [DF_MST_Student_Created]  DEFAULT (getdate()) FOR [Created]
+GO
+ALTER TABLE [dbo].[MST_Student] ADD  CONSTRAINT [DF_MST_Student_Modified]  DEFAULT (getdate()) FOR [Modified]
+GO
+ALTER TABLE [dbo].[LOC_City]  WITH CHECK ADD  CONSTRAINT [FK_LOC_City_LOC_Country] FOREIGN KEY([CountryID])
+REFERENCES [dbo].[LOC_Country] ([CountryID])
+GO
+ALTER TABLE [dbo].[LOC_City] CHECK CONSTRAINT [FK_LOC_City_LOC_Country]
+GO
+ALTER TABLE [dbo].[LOC_City]  WITH CHECK ADD  CONSTRAINT [FK_LOC_City_LOC_State] FOREIGN KEY([StateID])
+REFERENCES [dbo].[LOC_State] ([StateID])
+GO
+ALTER TABLE [dbo].[LOC_City] CHECK CONSTRAINT [FK_LOC_City_LOC_State]
+GO
+ALTER TABLE [dbo].[LOC_State]  WITH CHECK ADD  CONSTRAINT [FK_LOC_State_LOC_Country] FOREIGN KEY([CountryID])
+REFERENCES [dbo].[LOC_Country] ([CountryID])
+GO
+ALTER TABLE [dbo].[LOC_State] CHECK CONSTRAINT [FK_LOC_State_LOC_Country]
+GO
+ALTER TABLE [dbo].[MST_Student]  WITH CHECK ADD  CONSTRAINT [FK_MST_Student_LOC_City] FOREIGN KEY([CityID])
+REFERENCES [dbo].[LOC_City] ([CityID])
+GO
+ALTER TABLE [dbo].[MST_Student] CHECK CONSTRAINT [FK_MST_Student_LOC_City]
+GO
+ALTER TABLE [dbo].[MST_Student]  WITH CHECK ADD  CONSTRAINT [FK_MST_Student_MST_Branch] FOREIGN KEY([BranchID])
+REFERENCES [dbo].[MST_Branch] ([BranchID])
+GO
+ALTER TABLE [dbo].[MST_Student] CHECK CONSTRAINT [FK_MST_Student_MST_Branch]
---
+++ ASP .NET/Demo_Project/Database/SelectProcedures.sql
@@ -0,0 +1,114 @@
+
+-- Create Procedure for Select all countries
+CREATE PROCEDURE [dbo].[PR_Country_SelectAll]
+AS
+
+SELECT [dbo].[LOC_Country].[CountryName]
+	  ,[dbo].[LOC_Country].[CountryName]
+      ,[dbo].[LOC_Country].[CountryCode]
+	  ,[dbo].[LOC_Country].[Created]
+	  ,[dbo].[LOC_Country].[Modified]
+
+FROM [dbo].[LOC_Country]
+ORDER BY [dbo].[LOC_Country].[CountryName]
+
+
+-- Create Procedure for Select all states
+CREATE PROCEDURE [dbo].[PR_State_SelectAll]
+AS
+
+SELECT [dbo].[LOC_State].[StateID]
+      ,[dbo].[LOC_State].[CountryID]
+	  ,[dbo].[LOC_State].[StateName]
+	  ,[dbo].[LOC_State].[StateCode]
+	  ,[dbo].[LOC_State].[Created]
+	  ,[dbo].[LOC_State].[Modified]
+
+FROM [dbo].[LOC_State]
+INNER JOIN [dbo].[LOC_Country]
+ON [dbo].[LOC_Country].[CountryID] = [dbo].[LOC_State].[CountryID]
+ORDER BY [dbo].[LOC_Country].[CountryName]
+        ,[dbo].[LOC_State].[StateName]
+
+
+-- Create Procedure for Select all cities
+CREATE PROCEDURE [dbo].[PR_City_SelectAll]
+AS
+
+SELECT [dbo].[LOC_City].[CityID]
+	  ,[dbo].[LOC_City].[CityName]
+	  ,[dbo].[LOC_City].[StateID]
+	  ,[dbo].[LOC_City].[CountryID]
+	  ,[dbo].[LOC_City].[Citycode]
+	  ,[dbo].[LOC_City].[CreationDate]
+	  ,[dbo].[LOC_City].[Modified]
+
+FROM [dbo].[LOC_City]
+INNER JOIN [dbo].[LOC_State]
+ON [dbo].[LOC_State].[StateID] = [dbo].[LOC_City].[StateID]
+INNER JOIN [dbo].[LOC_Country] 
+ON [dbo].[LOC_Country].[CountryID] = [dbo].[LOC_State].[CountryID]
+ORDER BY [dbo].[LOC_Country].[CountryName]
+        ,[dbo].[LOC_State].[StateName]
+		,[dbo].[LOC_City].[CityName]
+
+
+
+-- Create Procedure for Select Country by PK
+CREATE PROCEDURE [dbo].[PR_Country_SelectByPK]
+@CountryID int
+AS
+
+SELECT [dbo].[LOC_Country].[CountryName]
+	  ,[dbo].[LOC_Country].[CountryName]
+      ,[dbo].[LOC_Country].[CountryCode]
+	  ,[dbo].[LOC_Country].[Created]
+	  ,[dbo].[LOC_Country].[Modified]
+
+FROM [dbo].[LOC_Country]
+WHERE [dbo].[LOC_Country].[CountryID] = @CountryID
+ORDER BY [dbo].[LOC_Country].[CountryName]
+
+
+-- Create Procedure for Select State by PK
+CREATE PROCEDURE [dbo].[PR_State_SelectByPK]
+@StateID int
+AS
+
+SELECT [dbo].[LOC_State].[StateID]
+      ,[dbo].[LOC_State].[CountryID]
+	  ,[dbo].[LOC_State].[StateName]
+	  ,[dbo].[LOC_State].[StateCode]
+	  ,[dbo].[LOC_State].[Created]
+	  ,[dbo].[LOC_State].[Modified]
+
+FROM [dbo].[LOC_State]
+INNER JOIN [dbo].[LOC_Country]
+ON [dbo].[LOC_Country].[CountryID] = [dbo].[LOC_State].[CountryID]
+WHERE [dbo].[LOC_State].[StateID] = @StateID
+ORDER BY [dbo].[LOC_Country].[CountryName]
+        ,[dbo].[LOC_State].[StateName]
+
+
+-- Create Procedure for Select City by PK
+CREATE PROCEDURE [dbo].[PR_City_SelectByPK]
+@CityID int
+AS
+
+SELECT [dbo].[LOC_City].[CityID]
+	  ,[dbo].[LOC_City].[CityName]
+	  ,[dbo].[LOC_City].[StateID]
+	  ,[dbo].[LOC_City].[CountryID]
+	  ,[dbo].[LOC_City].[Citycode]
+	  ,[dbo].[LOC_City].[CreationDate]
+	  ,[dbo].[LOC_City].[Modified]
+
+FROM [dbo].[LOC_City]
+INNER JOIN [dbo].[LOC_State]
+ON [dbo].[LOC_State].[StateID] = [dbo].[LOC_City].[StateID]
+INNER JOIN [dbo].[LOC_Country] 
+ON [dbo].[LOC_Country].[CountryID] = [dbo].[LOC_State].[CountryID]
+WHERE [dbo].[LOC_City].[CityID] = @CityID
+ORDER BY [dbo].[LOC_Country].[CountryName]
+        ,[dbo].[LOC_State].[StateName]
+		,[dbo].[LOC_City].[CityName]

4f8981ad376f97e11d6b3d816ef31893fe73a697
Uttam Nagvadiya <uttam.git835@gmail.com>
2023-09-03 18:59:36 +0530

    .

--- ASP .NET/Demo_Project/Database/SelectProcedures.sql
+++ ASP .NET/Demo_Project/Database/SelectProcedures.sql
@@ -3,7 +3,6 @@
 -- 1. Create Procedure for Select all countries
 CREATE OR ALTER PROCEDURE [dbo].[PR_Country_SelectAll]
 AS
-
 SELECT [dbo].[LOC_Country].[CountryID]
 	  ,[dbo].[LOC_Country].[CountryName]
       ,[dbo].[LOC_Country].[CountryCode]
@@ -16,15 +15,13 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 
 -- 2. Create Procedure for Select Country by PK
 CREATE OR ALTER PROCEDURE [dbo].[PR_Country_SelectByPK]
-	@CountryID int
+	@CountryID	int
 AS
-
 SELECT [dbo].[LOC_Country].[CountryID]
 	  ,[dbo].[LOC_Country].[CountryName]
       ,[dbo].[LOC_Country].[CountryCode]
 	  ,[dbo].[LOC_Country].[Created]
 	  ,[dbo].[LOC_Country].[Modified]
-
 FROM [dbo].[LOC_Country]
 WHERE [dbo].[LOC_Country].[CountryID] = @CountryID
 ORDER BY [dbo].[LOC_Country].[CountryName]
@@ -33,8 +30,7 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 -- 3. Create Insert Procedure to add any new record for Country
 CREATE OR ALTER PROCEDURE [dbo].[PR_Country_Insert_Record]
 	@CountryName	varchar(100),
-	@CountryCode	varchar(50)
-
+	@CountryCode	varchar(100)
 AS
 INSERT INTO [dbo].[LOC_Country]
 (
@@ -50,42 +46,35 @@ VALUES
 
 -- 4. Create Update Procedure to edit/modeify existing record for Country.
 CREATE OR ALTER PROCEDURE [dbo].[PR_Country_UpdateByPK]
-	@CountryID		int,
+	@CountryID				 int,
 	@CountryName	varchar(100),
-	@CountryCode	varchar(50)
-
+	@CountryCode	varchar(100)
 AS
 UPDATE [dbo].[LOC_Country]
-	
 	SET  [dbo].[LOC_Country].[CountryName] = @CountryName
 		,[dbo].[LOC_Country].[CountryCode] = @CountryCode
 		,[dbo].[LOC_Country].[Modified] = GETDATE()
-
 	WHERE [dbo].[LOC_Country].[CountryID] = @CountryID
 
 
 -- 5. Create Delete Procedure to delete record into Country Table.
 CREATE OR ALTER PROCEDURE [dbo].[PR_Country_DeleteByPK]
 	@CountryID	int
-
 AS
-
 DELETE FROM [dbo].[LOC_Country]
 WHERE [dbo].[LOC_Country].[CountryID] = @CountryID
 
 
 -- 6. Search Country by Country Name or Country Code.
 CREATE OR ALTER PROCEDURE [dbo].[PR_Country_Search]
-	@CountryName	varchar(150) = null,
-	@CountryCode	varchar(25) = null
+	@CountryName	varchar(100) = null,
+	@CountryCode	varchar(100) = null
 AS
-
 SELECT [dbo].[LOC_Country].[CountryID]
 	  ,[dbo].[LOC_Country].[CountryName]
       ,[dbo].[LOC_Country].[CountryCode]
 	  ,[dbo].[LOC_Country].[Created]
 	  ,[dbo].[LOC_Country].[Modified]
-
 FROM [dbo].[LOC_Country]
 WHERE [dbo].[LOC_Country].[CountryName] LIKE CONCAT('%', @CountryName, '%')
 AND [dbo].[LOC_Country].[CountryCode] LIKE CONCAT('%', @CountryCode, '%')
@@ -109,14 +98,12 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 -- 1. Create Procedure for Select all states
 CREATE OR ALTER PROCEDURE [dbo].[PR_State_SelectAll]
 AS
-
 SELECT [dbo].[LOC_State].[StateID]
       ,[dbo].[LOC_Country].[CountryName]
 	  ,[dbo].[LOC_State].[StateName]
 	  ,[dbo].[LOC_State].[StateCode]
 	  ,[dbo].[LOC_State].[Created]
 	  ,[dbo].[LOC_State].[Modified]
-
 FROM [dbo].[LOC_State]
 INNER JOIN [dbo].[LOC_Country]
 ON [dbo].[LOC_Country].[CountryID] = [dbo].[LOC_State].[CountryID]
@@ -126,16 +113,14 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 
 -- 2. Create Procedure for Select State by PK
 CREATE OR ALTER PROCEDURE [dbo].[PR_State_SelectByPK]
-	@StateID int
+	@StateID	int
 AS
-
 SELECT [dbo].[LOC_State].[StateID]
       ,[dbo].[LOC_State].[CountryID]
 	  ,[dbo].[LOC_State].[StateName]
 	  ,[dbo].[LOC_State].[StateCode]
 	  ,[dbo].[LOC_State].[Created]
 	  ,[dbo].[LOC_State].[Modified]
-
 FROM [dbo].[LOC_State]
 INNER JOIN [dbo].[LOC_Country]
 ON [dbo].[LOC_Country].[CountryID] = [dbo].[LOC_State].[CountryID]
@@ -146,10 +131,9 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 
 -- 3. Create Insert Procedure to add new record for State.
 CREATE OR ALTER PROCEDURE [dbo].[PR_State_Insert_Record]
+	@CountryID			 int,
 	@StateName	varchar(100),
-	@CountryID	int,
-	@StateCode	varchar(50)
-
+	@StateCode	varchar(100)
 AS
 INSERT INTO [dbo].[LOC_State]
 (
@@ -167,45 +151,38 @@ VALUES
 
 -- 4. Create Update Procedure to edit/modify existing record for State.
 CREATE OR ALTER PROCEDURE [dbo].[PR_State_UpdateByPK]
-	@StateID	int,
+	@StateID			 int,
+	@CountryID			 int,
 	@StateName	varchar(100),
-	@CountryID	int,
-	@StateCode	varchar(50)
-
+	@StateCode	varchar(100)
 AS
 UPDATE [dbo].[LOC_State]
-
 	SET  [dbo].[LOC_State].[StateName] = @StateName
 		,[dbo].[LOC_State].[CountryID] = @CountryID
 		,[dbo].[LOC_State].[StateCode] = @StateCode
 		,[dbo].[LOC_State].[Modified] = GETDATE()
-
 	WHERE [dbo].[LOC_State].[StateID] = @StateID
 
 
 -- 5. Create Delete Procedure to delete record into State Table.
 CREATE OR ALTER PROCEDURE [dbo].[PR_State_DeleteByPK]
 	@StateID	int
-
 AS
-
 DELETE FROM [dbo].[LOC_State]
 WHERE [dbo].[LOC_State].[StateID] = @StateID
 
 
 -- 6. Search State by State Name or State Code.
 CREATE OR ALTER PROCEDURE [dbo].[PR_State_Search]
-	@StateName	varchar(150) = null,
-	@StateCode	varchar(25) = null
+	@StateName	varchar(100) = null,
+	@StateCode	varchar(100) = null
 AS
-
 SELECT [dbo].[LOC_State].[StateID]
       ,[dbo].[LOC_Country].[CountryName]
 	  ,[dbo].[LOC_State].[StateName]
 	  ,[dbo].[LOC_State].[StateCode]
 	  ,[dbo].[LOC_State].[Created]
 	  ,[dbo].[LOC_State].[Modified]
-
 FROM [dbo].[LOC_State]
 INNER JOIN [dbo].[LOC_Country]
 ON [dbo].[LOC_Country].[CountryID] = [dbo].[LOC_State].[CountryID]
@@ -232,7 +209,6 @@ ORDER BY [dbo].[LOC_State].[StateName]
 -- 1. Create Procedure for Select all cities
 CREATE OR ALTER PROCEDURE [dbo].[PR_City_SelectAll]
 AS
-
 SELECT [dbo].[LOC_City].[CityID]
 	  ,[dbo].[LOC_City].[CityName]
 	  ,[dbo].[LOC_City].[Citycode]
@@ -242,7 +218,6 @@ SELECT [dbo].[LOC_City].[CityID]
 	  ,[dbo].[LOC_Country].[CountryName]
 	  ,[dbo].[LOC_City].[CreationDate]
 	  ,[dbo].[LOC_City].[Modified]
-
 FROM [dbo].[LOC_City]
 INNER JOIN [dbo].[LOC_State]
 ON [dbo].[LOC_State].[StateID] = [dbo].[LOC_City].[StateID]
@@ -255,9 +230,8 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 
 -- 2. Create Procedure for Select City by PK
 CREATE OR ALTER PROCEDURE [dbo].[PR_City_SelectByPK]
-	@CityID int
+	@CityID	 int
 AS
-
 SELECT [dbo].[LOC_City].[CityID]
 	  ,[dbo].[LOC_City].[CityName]
 	  ,[dbo].[LOC_City].[StateID]
@@ -267,7 +241,6 @@ SELECT [dbo].[LOC_City].[CityID]
 	  ,[dbo].[LOC_City].[Citycode]
 	  ,[dbo].[LOC_City].[CreationDate]
 	  ,[dbo].[LOC_City].[Modified]
-
 FROM [dbo].[LOC_City]
 INNER JOIN [dbo].[LOC_State]
 ON [dbo].[LOC_State].[StateID] = [dbo].[LOC_City].[StateID]
@@ -281,11 +254,10 @@ ORDER BY [dbo].[LOC_Country].[CountryName]
 
 -- 3. Create Insert Procedure to add new record for City.
 CREATE OR ALTER PROCEDURE [dbo].[PR_City_Insert_Record]
+	@StateID			 int,
+	@CountryID			 int,
 	@CityName	varchar(100),
-	@StateID	int,
-	@CountryID	int,
-	@CityCode	varchar(50)
-
+	@CityCode	varchar(100)
 AS
 INSERT INTO [dbo].[LOC_City]
 (
@@ -305,40 +277,34 @@ VALUES
 
 -- 4. Create Update Procedure to edit/modify existing record for City.
 CREATE OR ALTER PROCEDURE [dbo].[PR_City_UpdateByPK]
-	@CityID		int,
+	@CityID				 int,
+	@StateID			 int,
+	@CountryID			 int,
 	@CityName	varchar(100),
-	@StateID	int,
-	@CountryID	int,
-	@CityCode	varchar(50)
-
+	@CityCode	varchar(100)
 AS
 UPDATE [dbo].[LOC_City]
-
 	SET  [dbo].[LOC_City].[CityName] = @CityName
 		,[dbo].[LOC_City].[StateID] = @StateID
 		,[dbo].[LOC_City].[CountryID] = @CountryID
 		,[dbo].[LOC_City].[Citycode] = @CityCode
 		,[dbo].[LOC_City].[Modified] = GETDATE()
-
 	WHERE [dbo].[LOC_City].[CityID] = @CityID
 
 
 -- 5. Create Delete Procedure to delete record into City Table.
 CREATE OR ALTER PROCEDURE [dbo].[PR_City_DeleteByPK]
-	@CityID	int
-
+	@CityID	 int
 AS
-
 DELETE FROM [dbo].[LOC_City]
 WHERE [dbo].[LOC_City].[CityID] = @CityID
 
 
 -- 6. Search City by City Name or City Code.
 CREATE OR ALTER PROCEDURE [dbo].[PR_City_Search]
-	@CityName	varchar(150) = null,
-	@CityCode	varchar(25) = null
+	@CityName	varchar(100) = null,
+	@CityCode	varchar(100) = null
 AS
-
 SELECT [dbo].[LOC_City].[CityID]
 	  ,[dbo].[LOC_City].[CityName]
 	  ,[dbo].[LOC_City].[Citycode]
@@ -348,7 +314,6 @@ SELECT [dbo].[LOC_City].[CityID]
 	  ,[dbo].[LOC_Country].[CountryName]
 	  ,[dbo].[LOC_City].[CreationDate]
 	  ,[dbo].[LOC_City].[Modified]
-
 FROM [dbo].[LOC_City]
 INNER JOIN [dbo].[LOC_State]
 ON [dbo].[LOC_State].[StateID] = [dbo].[LOC_City].[StateID]
@@ -379,7 +344,7 @@ ORDER BY [dbo].[MST_Branch].[BranchName]
 
 -- 2. Create Procedure for Select Branch by PK.
 CREATE OR ALTER PROCEDURE [dbo].[PR_Branch_SelectByPK]
-	@BranchID int
+	@BranchID	int
 AS
 SELECT  [dbo].[MST_Branch].[BranchName]
 	   ,[dbo].[MST_Branch].[BranchCode]
@@ -392,8 +357,8 @@ ORDER BY [dbo].[MST_Branch].[BranchName]
 
 -- 3. Create Insert Procedure to add new record for Branch.
 CREATE OR ALTER PROCEDURE [dbo].[PR_Branch_Insert_Record]
-	@BranchName varchar(100),
-	@BranchCode varchar(100)
+	@BranchName	varchar(100),
+	@BranchCode	varchar(100)
 AS
 INSERT INTO [dbo].[MST_Branch]
 (
@@ -409,16 +374,14 @@ VALUES
 
 -- 4. Create Update Procedure to edit/modify existing record for Branch.
 CREATE OR ALTER PROCEDURE [dbo].[PR_Branch_UpdateByPK]
-	@BranchID	int,
-	@BranchName varchar(100),
+	@BranchID			 int,
+	@BranchName	varchar(100),
 	@BranchCode varchar(100)
 AS
 UPDATE [dbo].[MST_Branch]
-
 	SET [dbo].[MST_Branch].[BranchName] = @BranchName,
 		[dbo].[MST_Branch].[BranchCode] = @BranchCode,
 		[dbo].[MST_Branch].[Modified] = GETDATE()
-
 	WHERE [dbo].[MST_Branch].[BranchID] = @BranchID
 
 
@@ -487,8 +450,8 @@ CREATE OR ALTER PROCEDURE [dbo].[PR_Student_Insert_Record]
 	@StudentName		varchar(100),
 	@MobileNoStudent	varchar(100),
 	@Email				varchar(100),
-	@BranchID			int,
-	@CityID				int
+	@BranchID					 int,
+	@CityID						 int
 AS
 INSERT INTO [dbo].[MST_Student]
 (
@@ -510,22 +473,20 @@ VALUES
 
 -- 4. Create Update Procedure to edit/modify existing record for Student.
 CREATE OR ALTER PROCEDURE [dbo].[PR_Student_UpdateByPK]
-	@StudentID			int,
+	@StudentID					 int,
+	@BranchID					 int,
+	@CityID						 int,
 	@StudentName		varchar(100),
 	@MobileNoStudent	varchar(100),
-	@Email				varchar(100),
-	@BranchID			int,
-	@CityID				int
+	@Email				varchar(100)
 AS
 UPDATE [dbo].[MST_Student]
-
 	SET [dbo].[MST_Student].[StudentName] = @StudentName,
 		[dbo].[MST_Student].[MobileNoStudent] = @MobileNoStudent,
 		[dbo].[MST_Student].[Email] = @Email,
 		[dbo].[MST_Student].[BranchID] = @BranchID,
 		[dbo].[MST_Student].[CityID] = @CityID,
 		[dbo].[MST_Student].[Modified] = GETDATE()
-
 	WHERE [dbo].[MST_Student].[StudentID] = @StudentID
 
 
